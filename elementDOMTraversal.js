f17738670a1be46298716396e15f4a793213bfec
Jeffrey Emerich Christian <15chrjef@gmail.com>
2017-03-29 06:35:42 -0700

    Create elementDOMTraversal.js

---
+++ elementDOMTraversal.js
@@ -0,0 +1,8 @@
+var div = document.body.firstElementChild
+var ul = div.nextElementSibling
+var johnLi = ul.firstElementChild
+var peteLi = johnLi.nextElementSibling 
+console.log(div)//<div>Users:</div>
+console.log(ul)// <ul>...</ul>
+console.log(johnLi)// <li>John</li>
+console.log(peteLi)// <li>Pete</li>
